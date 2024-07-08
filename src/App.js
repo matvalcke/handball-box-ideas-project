a27b78be471c7f37d4b3be336e561312a2f3d1e3
matvalcke <mvalcke@adin.pro>
2024-07-08 15:20:56 +0200

    Offline mode and singleton delete

--- src/App.js
+++ src/App.js
@@ -2,26 +2,22 @@ import React, { useEffect } from 'react';
 import './App.css';
 import SuggestionsList from './components/SuggestionsList.tsx';
 import AddSuggestion from './components/AddSuggestion.tsx';
-import { ServiceWorkerManager } from "./service-worker-manager.ts";
 
 const App = () => {
     useEffect(() => {
-        async function setupServiceWorker() {
-            try {
-                const serviceWorkerManager = await ServiceWorkerManager.getInstance().register();
-                console.log('Service Worker registered:', serviceWorkerManager);
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('/service-worker.js')
+                .then(registration => {
+                    console.log('Service Worker registered:', registration);
 
-                if ('serviceWorker' in navigator) {
                     navigator.serviceWorker.addEventListener('message', (event) => {
                         console.log('Received message from service worker:', event.data);
                     });
-                }
-            } catch (error) {
-                console.error('Failed to register Service Worker:', error);
-            }
+                })
+                .catch(error => {
+                    console.error('Service Worker registration failed:', error);
+                });
         }
-
-        setupServiceWorker();
     }, []);
 
     function urlBase64ToUint8Array(base64String) {
